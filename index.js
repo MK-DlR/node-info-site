e15169a01567ebb87f63195775f63616f82a04d0
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-11-01 10:29:25 -0400

    initial deployment

---
+++ index.js
@@ -0,0 +1,49 @@
+// index.js
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  // get path user is visiting
+  let path = "./pages/";
+
+  // switch statement to handle page selection
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404; // error code
+      break;
+  }
+
+  // send an html file
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+// run on 3000 since 8080 is already used by another application
+server.listen(3000, "localhost", () => {
+  console.log("listening for requests on port 3000");
+});
